2a314feb1a104a0c16eb6cf5e7fbcf5a629ea5b8
Andreas Brachold <vdr07@deltab.de>
2008-03-29 16:08:57 +0000

    * Update header

--- libimage/pnm.c
+++ libimage/pnm.c
@@ -1,24 +1,12 @@
 /***************************************************************************
  * pnm.c
- * (C) Copyright  2004 Andreas Brachold    <anbr at users.berlios.de>
+ * (C) 2004-2008 Andreas Brachold    <anbr at users.berlios.de>
  *     based on works from Fabian E. Bustamante <fabianb-at-cs.umd.edu>
  *  Created: Thu Aug  7 2004
- ****************************************************************************/
-
-/*
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Library General Public License for more details.
+ * This code is distributed under the terms and conditions of the
+ * GNU GENERAL PUBLIC LICENSE. See the file COPYING for details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
 #include <stdio.h>
